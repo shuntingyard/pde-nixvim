54dfef63413903701a9469476fbd97010d300b8b
Tobias Frei <shuntingyard@gmail.com>
2024-11-30 17:28:40 +0100

    try setting system in modules' config 3

--- modules/nixos.nix
+++ modules/nixos.nix
@@ -1,44 +1,47 @@
 {
-  flake.nixosModules.default = {
-    config,
-    lib,
-    pkgs,
-    ...
-  }:
-    with lib; let
-      cfg = config.pde.nixos;
-    in {
-      options = {
-        pde.nixos = {
-          enable = mkEnableOption "Enables pde-nixvim system-wide";
-          # can we get the system here?
-          /*
+  flake.nixosModules.default =
+    { config
+    , lib
+    , pkgs
+    , ...
+    }:
+      with lib; let
+        cfg = config.pde.nixos;
+      in
+      {
+        options = {
+          pde.nixos = {
+            enable = mkEnableOption "Enables pde-nixvim system-wide";
+            # can we get the system here?
+            /*
           system = lib.mkOption {
             type = lib.types.str;
             default = "x86_64-linux";
             description = lib.mdDoc "System for derivation";
           };
-          */
-          colorscheme = lib.mkOption {
-            type = lib.types.str;
-            default = "darkblue";
-            description = lib.mdDoc "Set nvim system colorscheme";
+            */
+            colorscheme = lib.mkOption {
+              type = lib.types.str;
+              default = "darkblue";
+              description = lib.mdDoc "Set nvim system colorscheme";
+            };
           };
         };
-      };
 
-      config = mkIf cfg.enable {
-        environment.systemPackages = let
-          # grab the base config
-          pde = config.packages.${pkgs.system}.default;
+        config = mkIf cfg.enable {
+          environment.systemPackages =
+            let
+              # grab the base config
+              pde = pkgs.system.default;
 
-          # test if you can extend
-          pde-extended =
-            pde.extend
-            {
-              colorscheme = "${cfg.colorscheme}";
-            };
-        in [pde-extended];
+              # test if you can extend
+              pde-extended =
+                pde.extend
+                  {
+                    colorscheme = "${cfg.colorscheme}";
+                  };
+            in
+            [ pde-extended ];
+        };
       };
-    };
 }
